cc06f87f5f1db040452544a90db4e03804f0c066
Anna <ann2902sa@gmail.com>
2024-05-08 15:05:52 +0800

    update

--- tests-examples/HW_22/new_file.spec.ts
+++ tests-examples/HW_22/new_file.spec.ts
@@ -1,14 +1,5 @@
-import { test, expect, request } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-// test('Check garage page', {tag: '@garagePageCheck'}, async ({page, request}) => {
-//     await page.goto("/panel/garage");
-//     await expect(page.locator("div.panel-page_heading h1")).toHaveText("Garage");
-//     await page.screenshot({ path: 'screenshot.png', fullPage: true});
-//     // const response = await responsePromise; // очікування виконня promise
-//     const cars = await request.get('/api/cars');
-//     expect(cars.ok()).toBeTruthy();
-//     console.log(await cars.text());
-// });
 test('mock response to change user profile', async ({page}) => {
     await page.route('api/users/profile', async(route) => {
         const json = {
